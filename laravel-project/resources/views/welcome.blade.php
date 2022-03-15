e07290ebb793673e1b208b7303774f8cee0414dd
Mahmoud217TR <faafet.mahmoud@gmail.com>
2022-03-15 17:24:33 +0200

    modificatons

--- laravel-project/resources/views/welcome.blade.php
+++ laravel-project/resources/views/welcome.blade.php
@@ -17,8 +17,9 @@
     <!-- Styles -->
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
     <style>
-      body{
+      main{
         background: url("{{ asset('images/background.jpg') }}");
+        background-size:cover;
       }
     </style>
 
@@ -29,61 +30,67 @@
   <div id="app">
     {{--start page--}}
 
-       {{--start navbar--}}
-       <div class="container-fluid">
-        <div class="row d-flex align-items-center p-2">
-          {{--Logo & NameProject--}}
-           <div class="col-12 col-sm-12 col-md-7 col-lg-8 d-flex pb-3">
-             {{--start Logo--}}                  
-              <a href="/" class="unstyled-anchor">
-                <div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-1 ps-3 ">
-                  @include('components.logo',['width'=>80,'height'=>80])
+       <main>
+        {{--start navbar--}}
+        <div class="container-fluid">
+          <div class="row d-flex align-items-center p-2">
+            {{--Logo & NameProject--}}
+            <div class="col-12 col-sm-12 col-md-7 col-lg-8 d-flex pb-3">
+              {{--start Logo--}}                  
+                <a href="/" class="unstyled-anchor">
+                  <div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-1 ps-3 ">
+                    @include('components.logo',['width'=>80,'height'=>80])
+                  </div>
+                </a>
+              {{--end logo--}} 
+
+              {{--Strat Name Project--}} 
+                <a href="/" class="unstyled-anchor">
+                  <div class="col-9 col-sm-3 col-md-5 col-lg-3 col-xl-3 pt-3 ps-2 ms-4">
+                    <P class="h2" id="TextInHeader">{{ config('app.name', 'Laravel') }}</P>
+                  </div>
+                </a>
+              {{--end Name--}}
+            </div>
+            {{----}}
+
+            {{--Buttons in header--}}
+            <div class="col-12 col-sm-5 col-md-4 col-lg-3 d-flex align-items-center justify-content-around">
+              <a href="#" class="TH unstyled-anchor">services</a>
+              <a href="#" class="TH unstyled-anchor">about us</a>
+            </div>
+            {{----}}
+
+            {{--Person icon--}}
+              <div class="col-12 col-sm-12 col-md-1 col-lg-1 d-flex align-items-center justify-content-around">
+                <div>
+                  <i class="bi bi-person mb-4 "></i>
                 </div>
-              </a>
-             {{--end logo--}} 
-
-             {{--Strat Name Project--}} 
-              <a href="/" class="unstyled-anchor">
-                <div class="col-9 col-sm-3 col-md-5 col-lg-3 col-xl-3 pt-3 ps-2 ms-4">
-                  <P class="h2" id="TextInHeader">{{ config('app.name', 'Laravel') }}</P>
-                </div>
-              </a>
-             {{--end Name--}}
-           </div>
-          {{----}}
-
-          {{--Buttons in header--}}
-           <div class="col-12 col-sm-5 col-md-4 col-lg-3 d-flex align-items-center justify-content-around">
-             <a href="#" class="TH unstyled-anchor">services</a>
-             <a href="#" class="TH unstyled-anchor">about us</a>
-           </div>
-          {{----}}
-
-          {{--Person icon--}}
-            <div class="col-12 col-sm-12 col-md-1 col-lg-1 d-flex align-items-center justify-content-around">
-              <div>
-                <i class="bi bi-person mb-4 "></i>
+              </div>
+            {{----}}
+        </div>
+        </div>
+        {{--end navbar--}}
+      
+        {{--text--}}
+        <div class="container ms-0 ps-5" id="ContentWelcome">
+          <div class="row">
+            <div class="col-8">
+              <div class="text-start pt-3 mb-4">
+                <p class="h1" id="Welcome">welcome to <span id="NameProject">{{ config('app.name', 'Laravel') }}</span></p> 
+              </div>
+              <div class="text-start ps-3 pt-3 ">
+                <p class="h4" id="Welcome">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, quae cupiditate nihil deleniti velit earum dolores dolore nam aut consequatur provident ducimus molestias ad iure officia inventore ipsam numquam quas esse praesentium rem. Itaque natus modi quis reprehenderit voluptate quas odio id quae nulla accusamus, aperiam dolor incidunt unde quidem.</p> 
+              </div>
+              <div class="text-start ps-2 pt-3">
+                <a href="#" class="btn Rigester TB mx-2">Register</a>
+                <a href="#" class="btn View TB mx-2">View</a>
               </div>
             </div>
-          {{----}}
-       </div>
-       </div>
-    {{--end navbar--}}
-    
-    {{--text--}}
-      <div class="container ps-5" id="ContentWelcome">
-      <div class="offset-lg-1 offset-xl-2 pt-3 ">
-        <p class="h1" id="Welcome">welcome to <span id="NameProject">{{ config('app.name', 'Laravel') }}</span></p> 
-      </div>
-      <div class="text-center ps-3 pt-3 ">
-        <p class="h4" id="Welcome">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, iusto illo, nostrum ab fugit est voluptate accusantium cumque at ipsa in. Magni minus libero debitis saepe iusto animi accusantium voluptates.</p> 
-      </div>
-      <div class="text-center ps-3 pt-3">
-        <a href="#" class="btn Rigester TB mx-2" type="button">Register</a>
-        <a href="#" class="btn View TB mx-2" type="button">View</a>
-      </div>
-      </div>
-    {{----}}
+          </div>
+        </div>
+        {{----}}
+       </main>
 
     {{--end page--}}
   </div>
