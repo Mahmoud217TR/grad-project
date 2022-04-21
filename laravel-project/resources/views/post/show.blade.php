34cb2aa85c8d01323fc46b773b4bf101f13cf1e0
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2022-04-22 01:53:31 +0300

    posts.show fully working

--- laravel-project/resources/views/post/show.blade.php
+++ laravel-project/resources/views/post/show.blade.php
@@ -4,6 +4,46 @@
 <div class="container pt-3">
     <div class="card">
         <div class="card-body bg-dark">
+			<div class="row">
+				<div class="col">
+					<div class="d-flex align-items-center">
+						<a href="{{ route('profile.show',$post->user->profile) }}" class="unstyled-anchor d-flex align-items-center">
+							<img class="rounded-circle" src="@if(!$post->user->profile->hasProfilePic()){{ asset('images/blank-profile-picture.svg') }}@else {{ asset('storage/'.$post->user->profile->profile_pic) }} @endif" width="60" height="60">
+							<div class="d-flex flex-column ms-3">
+								<p class="orange-text base-line mb-0">{{ $post->user->name }}</p>
+							</div>
+						</a>
+					</div>
+				</div>
+				@canany(['update','delete'],$post)
+				<remove-component 
+					hidden='hidden'
+					button-id ='remove-post-{{ $post->id }}'
+					text="Remove"
+					action="{{ route('post.destroy',$post) }}"
+					title="Removing {{ $post->title }}"
+					msg="Are you Sure you want to REMOVE {{ $post->title }}??"
+				></remove-component>
+				<div class="col-sm-4 d-flex justify-content-end dropstart">
+                    <a class="orange-text unstyled-anchor base-line" type="button" 
+                        data-bs-toggle="dropdown" id="#ff"
+                        aria-expanded="false">
+
+                        <i class="bi bi-three-dots"></i>
+                    </a>
+                    <div class="col col-md-6 mx-2 card collapse text-dark dropdown-menu" aria-labelledby="ff">
+                        <ul class="m-3 list-unstyled">
+							@can('update',$post)
+                            	<a class="unstyled-anchor1" href="{{ route('post.edit',$post) }}"><li class="border-bottom mb-2" type="button"><i class="orange-text TB bi bi-pencil-square mx-1"></i>Edite</li></a>
+							@endcan
+                            @can('delete',$post)
+								<li class="mb-2" type="button" onclick="event.preventDefault();document.getElementById('remove-post-{{ $post->id }}').click();"><i class="orange-text TB bi bi-trash mx-1"></i>Delete</li>
+							@endcan
+                        </ul>
+                    </div>
+                </div>
+				@endcan
+			</div>
             <div class="row">
                 <div class="col-8">
                     <p class="base-line">
@@ -16,21 +56,6 @@
                          <div class="">
                             <p>{{ $post->created_at->format('y-M-d h:m:s a') }}</p> 
                          </div>
-                    </div>
-                </div>
-                <div class="col-4 d-flex justify-content-end dropstart">
-                    <a class="orange-text unstyled-anchor base-line" type="button" 
-                        data-bs-toggle="dropdown" id="#ff"
-                        aria-expanded="false">
-
-                        <i class="bi bi-three-dots"></i>
-                    </a>
-                    <div class="col col-md-6 mx-2 card collapse text-dark dropdown-menu" aria-labelledby="ff">
-                        <ul class="m-3 list-unstyled">
-                            <li class="border-bottom mb-2" type="button"><i class="orange-text TB bi bi-save2 mx-1"></i> Save</li>
-                            <a class="unstyled-anchor1" href="/post-create"><li class="border-bottom mb-2" type="button"><i class="orange-text TB bi bi-pencil-square mx-1"></i>Edite</li></a>
-                            <li class="border-bottom mb-2" type="button"><i class="orange-text TB bi bi-trash mx-1"></i> Delete</li>
-                        </ul>
                     </div>
                 </div>
             </div>
@@ -82,7 +107,7 @@
 						<div class="col-10 p-3 border-bottom">
 							{{--name person--}}
 							<div class="d-flex">
-								<img class="rounded-circle" src="{{ asset('images/background.jpg') }}" width="60" height="60">
+								<img class="rounded-circle" src="@if(!$comment->user->profile->hasProfilePic()){{ asset('images/blank-profile-picture.svg') }}@else {{ asset('storage/'.$comment->user->profile->profile_pic) }} @endif" width="60" height="60">
 								<div class="d-flex flex-column ms-3">
 									<p class="orange-text base-line mb-0">{{ $comment->user->name }}</p>
 									<p class="text-secondary">{{ $comment->created_at->format('y-M-d h:m:s a') }}</p> 
@@ -91,8 +116,8 @@
 							{{--end name person--}}
 
 							{{--comment person--}}
-							<div class="mt-2">
-								<h6 class="text-Secondary">{!! $comment->content !!}</h6>
+							<div class="mt-2 text-Secondary" style="text-indent: 2rem">
+								{!! $comment->content !!}
 							</div>
 							{{--end comment person--}}
 
