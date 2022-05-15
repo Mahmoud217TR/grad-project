f4bab050afd54dff69023d185536df85a4ae2ad6
maya <maokda892@gmail.com>
2022-05-15 19:58:15 +0300

    reports

--- laravel-project/resources/views/layouts/app.blade.php
+++ laravel-project/resources/views/layouts/app.blade.php
@@ -38,45 +38,7 @@
         @include('layouts.nav')
       {{--end navbar--}}
       {{--content--}}
-
-
-      <div class="container">
-
-        <div class="row">
-          <div class="col text-center pyt-1">
-          <h1 class="orange-text pt-5">WE RECIEVE YOUR REPORT</h1>
-          <h2 class="orange-text pt-5">THANK YOU DARLING</h2>
-          </div>
-        </div>
-        <div class="row g-3 ">
-          <div class="col-3 "></div>
-
-          <div class="col-6 justify-content-center pt-5">
-            <a href="{{ route('welcome') }}" class="unstyled-anchor">
-              @include('components.SVG19',['width'=>80,'height'=>80])
-            </a>
-          </div>
-
-          <div class="col-3"></div>
       
-        </div>
-
-        <div class="row g-3 ">
-          <div class="col-4 "></div>
-
-          <div class="col-4">
-              <button type="submit" class="btnReport button-primary TB box">
-                  {{ ('go to home page') }}
-              </button>
-          </div>
-
-          <div class="col-4"></div>
-      
-        </div>
-    
-
-
-      </div>
         <main class="my-4">
             @yield('content')
         </main>
--- laravel-project/resources/views/layouts/recieve.blade.php
+++ laravel-project/resources/views/layouts/recieve.blade.php
@@ -1,38 +1,40 @@
 {{--receiving message--}}
 <div class="container">
 
-    <div class="row">
-      <div class="col text-center pyt-1">
-      <h1 class="orange-text pt-5">WE RECIEVE YOUR REPORT</h1>
-      <h2 class="orange-text pt-5">THANK YOU DARLING</h2>
-      </div>
+  <div class="row">
+    <div class="col text-center pyt-3">
+    <h1 class="orange-text pt-5">WE RECIEVE YOUR REPORT</h1>
+    <h2 class="orange-text pt-5">THANK YOU DARLING</h2>
     </div>
-    <div class="row g-3 ">
-      <div class="col-3 "></div>
+  </div>
+  <div class="row g-3 ">
+    <div class="col-2 "></div>
 
-      <div class="col-6 justify-content-center pt-5">
+    <div class="col-8 pt-5">
+      <div class="row justify-content-center">
         <a href="{{ route('welcome') }}" class="unstyled-anchor">
-          @include('components.SVG19',['width'=>80,'height'=>80])
+         @include('components.SVG19',['width'=>80,'height'=>80])
         </a>
       </div>
-
-      <div class="col-3"></div>
-  
     </div>
 
-    <div class="row g-3 ">
-      <div class="col-4 "></div>
-
-      <div class="col-4">
-          <button type="submit" class="btnReport button-primary TB box">
-              {{ ('go to home page') }}
-          </button>
-      </div>
-
-      <div class="col-4"></div>
-  
-    </div>
-
-
+    <div class="col-2"></div>
 
   </div>
+
+  <div class="row g-3 ">
+    <div class="col-4 "></div>
+
+    <div class="col-4 ">
+       <div class="row justify-content-center"> <button type="submit" class="btnReport button-primary TB box ">
+            {{ ('go to home page') }}
+        </button></div>
+    </div>
+
+    <div class="col-4"></div>
+
+  </div>
+
+
+
+</div>
